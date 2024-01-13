b1553c4b569515d82f1af95b11a0e28b0a35cb00
s2308 <s2308@matsue-ct.jp>
2024-01-14 01:09:02 +0900

    '出題まで'

---
+++ Assets/Scripts/csvReader.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System;
+
+public class csvReader : MonoBehaviour
+{
+    TextAsset csvFile; // CSVファイル
+    List<string[]> csvData = new List<string[]>(); // CSVファイルの中身を入れるリスト
+
+    private int qnum; //問題番号
+    public Text quest; //問題文
+    public Text[] choices = new Text[4]; //
+    public Text ans; //答え
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        csvFile = Resources.Load("quiz") as TextAsset; // ResourcesにあるCSVファイルを格納
+        StringReader reader = new StringReader(csvFile.text); // TextAssetをStringReaderに変換
+
+        while (reader.Peek() != -1)
+        {
+            string line = reader.ReadLine(); // 1行ずつ読み込む
+            csvData.Add(line.Split(',')); // csvDataリストに追加する
+        }
+
+        System.Random rand = new System.Random();
+        qnum = rand.Next(0, csvData.Count);//ランダムに問題決定
+    }
+
+    private void Update()
+    {
+        quest.text = string.Format("{0}", csvData[qnum][0]); //問題文
+        for (int i = 0; i < 4; i++) //選択肢
+        {
+            choices[i].text = string.Format(csvData[qnum][i + 1]);
+        }
+        ans.text = string.Format("{0}", csvData[qnum][5]); //問題文
+    }
+}
